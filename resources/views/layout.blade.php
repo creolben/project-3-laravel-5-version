50e41ec1d85696b1489937a388f7f9b5ce543b41
Ben Jerome <benjaminjjerome@gmail.com>
2016-10-26 20:28:38 -0400

    fix header

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,7 @@
 <link href='http://fonts.googleapis.com/css?family=Poppins:400,600,700,500,300' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Roboto:400,900italic,900,700italic,700,400italic,500,500italic,300,100italic,100,300italic' rel='stylesheet' type='text/css'>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>Developer's Best Friend</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
