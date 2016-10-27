9124d2eaf534e71ef2006f73c908dc6432aab5c7
Ben Jerome <benjaminjjerome@gmail.com>
2016-10-26 20:21:13 -0400

    fix header

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,17 @@
 <link href="css/lightbox.css" rel="stylesheet">
 <link href='http://fonts.googleapis.com/css?family=Poppins:400,600,700,500,300' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Roboto:400,900italic,900,700italic,700,400italic,500,500italic,300,100italic,100,300italic' rel='stylesheet' type='text/css'>
-  @extends('header')
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Developer's Best Friend</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
   <body>
     <header class="header">
     	<div class="container">
