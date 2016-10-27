52b9f3f4206c850a47f585c3c7d649932c67d984
Ben Jerome <benjaminjjerome@gmail.com>
2016-10-26 20:35:22 -0400

    fix header

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,8 +3,8 @@
 <link href="css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="style.css" type="text/css">
 <link href="css/lightbox.css" rel="stylesheet">
-<link href='http://fonts.googleapis.com/css?family=Poppins:400,600,700,500,300' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Roboto:400,900italic,900,700italic,700,400italic,500,500italic,300,100italic,100,300italic' rel='stylesheet' type='text/css'>
+<link href="http://fonts.googleapis.com/css?family=Poppins:400,600,700,500,300" rel="stylesheet" type="text/css">
+<link href="http://fonts.googleapis.com/css?family=Roboto:400,900italic,900,700italic,700,400italic,500,500italic,300,100italic,100,300italic" rel="stylesheet" type="text/css">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>Developer's Best Friend</title>
