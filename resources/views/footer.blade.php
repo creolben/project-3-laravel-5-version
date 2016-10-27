480c6ca0f192d1e5e3c2f70a70a9f553e9835a16
Ben Jerome <benjaminjjerome@gmail.com>
2016-10-26 20:45:35 -0400

    fix header

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,7 +2,7 @@
 <div class="container text-center">
   <div class="row">
 		<div class="col-md-12">
-			<p>Copyright &copy; Benjamin Jerome 2016 By <a href="http://p1.cs15class.online/">HTML5 Layouts</a> | <a href="http://vectortoons.com/">Get Vector Graphics</a></p>
+			<p>Copyright &copy; Benjamin Jerome 2016 By <a href="http://p1.cs15class.online/">CS15 Class</a></p>
 		</div>
 	</div>
 </div>
